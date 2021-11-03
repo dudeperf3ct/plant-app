15c08e80090111cff4ba1655ecb4ea84b9080fa8
shubham <shubham@smartcow.ai>
2021-11-03 15:04:39 +0530

    add plant.id api

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import streamlit as st
 import requests
 import json
 import pandas as pd
+from pprint import pprint
+import http.client
+import urllib.request, urllib.parse, urllib.error
+import base64
+
 
 def main():
 
@@ -13,7 +18,7 @@ def main():
         Image = Image.read()
         st.image(Image)
 
-    menu = ['None', 'Pl@ntNet API']
+    menu = ['None', 'Pl@ntNet API', 'Plant.id API', 'Azure API']
     st.sidebar.header('API Selection')
     choice = st.sidebar.selectbox('Select any API', menu)
 
@@ -60,9 +65,89 @@ def main():
                     })
                 st.table(df)
             else:
-                st.write('Querying failed...')
+                st.write(f'Querying failed with response status code {response.status_code}')
 
+    elif choice == 'Azure API' and Image is not None:
+        st.write('Waiting for api key')
+        topK, predictMode = 5, 'classifyAndDetect'
 
+        headers = {
+            # Request headers
+            'Content-Type': 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key': '{subscription key}',
+        }
+
+        params = urllib.parse.urlencode({
+            # Request parameters
+            'topK': topK,
+            'predictMode': predictMode,
+        })
+
+        try:
+            conn = http.client.HTTPSConnection('aiforearth.azure-api.net')
+            conn.request("POST", "/species-classification/v2.0/predict?%s" % params, "{Image}", headers)
+            response = conn.getresponse()
+            data = response.read()
+            print(data)
+            conn.close()
+        except Exception as e:
+            print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+    elif choice == 'Plant.id API' and Image is not None:
+        st.write("Querying Plant.id API")
+
+        params = {
+            "api_key": st.secrets['plantid_key'],
+            "images": [base64.b64encode(Image).decode("ascii")],
+            "modifiers": ["crops_fast"],
+            "plant_language": "en",
+            "plant_details": ["common_names",
+                                "edible_parts",
+                                "gbif_id"
+                                "name_authority",
+                                "propagation_methods",
+                                "synonyms",
+                                "structured_name",
+                                "taxonomy",
+                                "url",
+                                "wiki_description",
+                                "wiki_image",
+                                ]
+            }
+
+        headers = {
+            "Content-Type": "application/json"
+            }
+
+        response = requests.post("https://api.plant.id/v2/identify",
+                                json=params,
+                                headers=headers)
+
+        json_result = response.json()
+
+        if response.status_code == 200:
+                scores, common_names, spec_names, genus_names, sci_names, taxo = [], [], [], [], [], []
+                st.write(f"Retriving top {len(json_result['suggestions'])} results")
+                for i in range(len(json_result['suggestions'])):
+                    scores.append(json_result['suggestions'][i]['probability'])
+                    common_names.append(json_result['suggestions'][i]['plant_details']['common_names'])
+                    sci_names.append(json_result['suggestions'][i]['plant_details']['scientific_name'])
+                    genus_names.append(json_result['suggestions'][i]['plant_details']['structured_name']['genus'])
+                    spec_names.append(json_result['suggestions'][i]['plant_details']['structured_name']['species'])
+                    taxo.append(json_result['suggestions'][i]['plant_details']['taxonomy'])
+
+                df = pd.DataFrame(
+                    {
+                        'scores' : scores,
+                        'commonNames': common_names,
+                        'scientificName': sci_names,
+                        'genus': genus_names,
+                        'species': spec_names,
+                        'taxonomy': taxo
+                    })
+                st.table(df)
+        else:
+            st.write(f'Querying failed with response status code {response.status_code}')
 
 if __name__ == '__main__':
     main()
